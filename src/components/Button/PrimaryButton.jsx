015be15268f9ce58ed66b1bb22b3955a7f31f57b
c0der <tgpop2000@gmail.com>
2024-07-04 00:09:46 +0300

    v2

---
+++ src/components/Button/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PrimaryButton = ({text}) => {
+    return (
+        <button className={'h-[40px] w-full text-white text-[14px] text-center bg-[#FF0037] rounded-[6px] hover:bg-[#CB002C] transition-all'}>{text}</button>
+    );
+};
+
+export default PrimaryButton;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+// IMG
+import logo from '../assets/logo_login.png'
+import PrimaryButton from "../components/Button/PrimaryButton";
+
+const Login = () => {
+    return (
+        <div className='flex min-h-screen justify-between'>
+            <div className='py-16 mx-auto max-w-[486px] w-full flex flex-col justify-between'>
+                <img alt={'logo'} src={logo} className={'max-w-[183px]'}/>
+                <div className={'flex flex-col gap-[36px]'}>
+                    <div>
+                        <h1 className='text-[#222225] text-[24px] font-semibold'>Добро пожаловать!</h1>
+                        <p className={'text-[#66666E] text-[14px] font-regular pt-[6px]'}>Войдите в свой аккаунт</p>
+                    </div>
+                    <div className={'flex flex-col gap-[12px]'}>
+                        <div>
+                            <label className={'text-[#66666E] text-[14px] font-medium'}>Логин</label>
+                            <div className='h-[40px] border rounded-[6px] border-[#E2E2E5] mt-[6px]'>
+                                <input className='outline-0 h-full px-2' placeholder={'Введите логин'}/>
+                            </div>
+                        </div>
+                        <div>
+                            <label className={'text-[#66666E] text-[14px] font-medium'}>Пароль</label>
+                            <div className='h-[40px] border rounded-[6px] border-[#E2E2E5] mt-[6px]'>
+                                <input className='outline-0 h-full px-2' placeholder={'Введите пароль'}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-[16px]'>
+                        <PrimaryButton text={'Войти'}/>
+                        <div className={'flex items-center justify-between text-[14px]'}>
+                            <p className={'text-[#66666E]'}>Нет аккаунта? <a href={'#'}className='text-[#FF0037]'>Регистрация</a></p>
+                            <a href={'#'} className='text-[#FF0037]'>Забыли пароль?</a>
+                        </div>
+                    </div>
+                </div>
+                <p className={'text-[#66666E] text-[12px]'}>
+                    Нажимая кнопку “Войти”, вы соглашаетесь с условиями
+                </p>
+            </div>
+            <div className='w-1/2 bg-[#FF0037]'>
+                <div className={'min-h-screen bg-cover bg-no-repeat background'}/>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
